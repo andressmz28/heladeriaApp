d7deac21d222f83d9fb86d545fa4b8a180056ff9
Andres Martinez <andressmz28@gmail.com>
2023-07-27 09:37:47 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PedidoComponent } from './pedido/pedido.component';
+import { VerPedidoComponent } from "./ver-pedido/ver-pedido.component";
+import { HacerPedidoComponent } from './hacer-pedido/hacer-pedido.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'hacer-pedido', component: HacerPedidoComponent },
+  { path: 'ver-pedido', component: VerPedidoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeladosComponent } from './helados/helados.component';
+import { HomeComponent } from './home/home.component';
+import { PedidoComponent } from './pedido/pedido.component';
+import { HacerPedidoComponent } from './hacer-pedido/hacer-pedido.component';
+import { VerPedidoComponent } from './ver-pedido/ver-pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeladosComponent,
+    HomeComponent,
+    PedidoComponent,
+    HacerPedidoComponent,
+    VerPedidoComponent
   ],
   imports: [
     BrowserModule,
