a65b50b23951d8ff0ce6421d39fa711ec1303082
Jocelyn Martinez <ijrmartinez142@gmail.com>
2023-08-02 22:13:20 -0500

    Menu en funcionamiento

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,15 +10,16 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     document.addEventListener("DOMContentLoaded", function () {
       const tabs = document.querySelectorAll(".nav-link");
-      const tabContents = document.querySelectorAll(".tab-content");
+      const tabContents = document.querySelectorAll(".tab-pane");
+      console.log(tabContents)
 
       tabs.forEach((tab, index) => {
         tab.addEventListener("click", () => {
           tabs.forEach((t) => t.classList.remove("active"));
-          tabContents.forEach((content) => content.classList.remove("active"));
+          tabContents.forEach((content) => content.classList.remove("active" , "show"));
 
           tab.classList.add("active");
-          tabContents[index].classList.add("active");
+          tabContents[index].classList.add("active" , "show");
         });
       });
     });
