2b52320cebaa4f4495236e118424babee98ca7c4
Jocelyn Martinez <ijrmartinez142@gmail.com>
2023-08-02 20:08:38 -0500

    single-nav-menu con funcionamiento

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  ngOnInit(): void {
+    document.addEventListener("DOMContentLoaded", function () {
+      const tabs = document.querySelectorAll(".nav-link");
+      const tabContents = document.querySelectorAll(".tab-content");
+
+      tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+          tabs.forEach((t) => t.classList.remove("active"));
+          tabContents.forEach((content) => content.classList.remove("active"));
+
+          tab.classList.add("active");
+          tabContents[index].classList.add("active");
+        });
+      });
+    });
+  }
 }
