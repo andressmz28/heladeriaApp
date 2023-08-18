6b2cf88e74cb96475cd282479c5d1ff0a1fc21a0
Jocelyn Martinez <ijrmartinez142@gmail.com>
2023-08-17 22:05:35 -0500

    Error resulto de carga Nav Bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
-  ngOnInit(): void {
-    document.addEventListener("DOMContentLoaded", function () {
-      const tabs = document.querySelectorAll(".nav-link");
-      const tabContents = document.querySelectorAll(".tab-pane");
-      console.log(tabContents)
+  ngAfterViewInit(): void {
+    const tabs = document.querySelectorAll(".nav-link");
+    const tabContents = document.querySelectorAll(".tab-pane");
+    console.log(tabContents)
 
-      tabs.forEach((tab, index) => {
-        tab.addEventListener("click", () => {
-          tabs.forEach((t) => t.classList.remove("active"));
-          tabContents.forEach((content) => content.classList.remove("active" , "show"));
+    tabs.forEach((tab, index) => {
+      tab.addEventListener("click", () => {
+        tabs.forEach((t) => t.classList.remove("active"));
+        tabContents.forEach((content) => content.classList.remove("active", "show"));
 
-          tab.classList.add("active");
-          tabContents[index].classList.add("active" , "show");
-        });
+        tab.classList.add("active");
+        tabContents[index].classList.add("active", "show");
       });
     });
   }
