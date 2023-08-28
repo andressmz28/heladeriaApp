ab6c99108b98a4f1ca587c31cc13aca4ff7febd2
Jocelyn Martinez <ijrmartinez142@gmail.com>
2023-08-28 00:23:44 -0500

    Se agregaron redes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,38 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  ngOnInit(): void {
+    const buttonContainer = document.getElementById("button-container");
+
+    if (buttonContainer) {
+      window.addEventListener("scroll", function () {
+        if (window.scrollY > 100) {
+          buttonContainer.classList.remove("hidden");
+        } else {
+          buttonContainer.classList.add("hidden");
+        }
+      });
+    }
+  }
+
+ 
+  redirectToFacebook() {
+    // Cambia esta URL por la que deseas redirigir
+    window.location.href = "https://www.facebook.com/profile.php?id=100085309147765&mibextid=LQQJ4d";
+  }
+
+
+  redirectToInstagram() {
+    // Cambia esta URL por la que deseas redirigir
+    window.location.href = "https://instagram.com/wow_gelato?igshid=OGQ5ZDc2ODk2ZA==";
+  }
+
 
   ngAfterViewInit(): void {
     const tabs = document.querySelectorAll(".nav-link");
@@ -22,4 +49,6 @@ export class HomeComponent implements AfterViewInit {
       });
     });
   }
+
+  // ... otros métodos y propiedades de la clase ...
 }
